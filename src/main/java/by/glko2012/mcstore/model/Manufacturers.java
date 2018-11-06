1f2f3888072550e0f2d39be305eb4b6dfda91e14
Gleb Kozinets <glebokator@gmail.com>
2018-11-06 19:59:08 +0300

    def

---
+++ src/main/java/by/glko2012/mcstore/model/Manufacturers.java
@@ -0,0 +1,32 @@
+package by.glko2012.mcstore.model;
+
+public class Manufacturers {
+    private String location;
+    private String name;
+    private String manuf_id;
+
+    //constructors   //getter-setters
+    public String setLocation(String location) {
+        return this.location = location;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public String setName(String name) {
+        return this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String setManuf_id(String manuf_id) {
+        return this.manuf_id = manuf_id;
+    }
+
+    public String getManuf_id() {
+        return this.manuf_id;
+    }
+}
